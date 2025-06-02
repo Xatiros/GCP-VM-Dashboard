1b1601c21962c4f6d2d11adfa917de08df13fa6c
Christian-Sanchez-IT <christian.sanchez@gemigeo.com>
2025-06-02 18:28:21 +0200

    fix: modificando enlace api

--- services/vmService.ts
+++ services/vmService.ts
@@ -1,7 +1,7 @@
 // src/services/vmService.ts
 import { VirtualMachine, VMStatus } from '../types';
 
-const API_BASE_URL = 'https://gcp-vm-dashboard-780691668337.europe-southwest1.run.app/api';
+const API_BASE_URL = 'https://gcp-vm-dashboard-backend-service-780691668337.europe-southwest1.run.app/api';
 
 // --- MODIFICAR FUNCIONES PARA ACEPTAR Y ENVIAR TOKEN ---
 export const fetchVMs = async (projectId: string, token: string): Promise<VirtualMachine[]> => {
